5f3836845cd06bb44aa1ea1cc8933d68cbab37fc
Vladimirs Ivanovs <ivanovs223@gmail.com>
2015-03-16 12:18:59 +0000

    Initial commit

---
+++ src/AppCodePoints.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+
+public class AppCodePoints {
+	public static void main(String[] args) {
+		testCodePoint(65);
+		testCodePoint(10_000);
+		testCodePoint(50_000);
+		testCodePoint(165_536);
+		
+	}
+	
+	private static void testCodePoint(int codePoint){
+		char[] charArray = Character.toChars(codePoint);
+		System.out.println("char[]: " + Arrays.toString(charArray));
+		String str = new String(charArray);
+		System.out.println("String: " + str);
+		System.out.println("String length: " + str.length());
+		System.out.println("String.codePointCount: " + str.codePointCount(0, str.length()));
+	}
+
+}
